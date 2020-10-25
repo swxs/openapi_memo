5d97a52623a04864de7a9d4e7a3ca07fb22a9b65
swxs <466565029@qq.com>
2020-10-25 12:31:41 +0800

    [swxs] feature: docker打包

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <el-container v-show="!login" class="login-block">
       <el-aside class="login-img img-block">
-        <img src="./static/login.png" />
+        <img src="./assets/images/login.png" />
       </el-aside>
       <el-main class="login-iframe">
         <iframe
@@ -14,8 +14,8 @@
         ></iframe>
       </el-main>
     </el-container>
+    <!--所有的页面都将加载到此处-->
     <router-view v-if="login"></router-view>
-    <!--所有的页面都将加载到此处,app.vue只提供一个容器-->
   </div>
 </template>
 
@@ -84,7 +84,9 @@ export default {
       }
     },
   },
-  created() {},
+  created() {
+    document.title = "便签";
+  },
   beforeMount() {
     // 在外部vue的window上添加postMessage的监听，并且绑定处理函数handleMessage
     window.addEventListener('message', this.handleMessage)
